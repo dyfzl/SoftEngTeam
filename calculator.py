346169f7bafb0f40fd83c8975cfe7541ad5b034c
ffe4el <codkan20@gmail.com>
2023-11-16 22:02:17 +0900

    이스터에그 오류 수정

--- calculator.py
+++ calculator.py
@@ -6,31 +6,49 @@ def main():
         return
     while True:
         operator = input()
-        if operator == '=':
+        if operator == 7532:
+            print("Software Engineering Class")
+            return
+        elif operator == '=':
             if not flag:
                 print('ERROR!')
             else:
                 print(a)
             break
-        if operator == "-" :
+        if operator == "-":
+            if sub(a) is None:
+                break
             a = sub(a)
         elif operator == "+":
+            if add(a) is None:
+                break
             a = add(a)
         elif operator == '*':
+            if mul(a) is None:
+                break
             a = mul(a)
         else:
             flag = False
 
 def sub(a):
     b = int(input())
+    if b == 7532:
+        print("Software Engineering Class")
+        return None
     return a - b
 
 def add(a):
     b = int(input())
+    if b == 7532:
+        print("Software Engineering Class")
+        return None
     return a + b
 
 def mul(a):
-    b = int (input())
+    b = int(input())
+    if b == 7532:
+        print("Software Engineering Class")
+        return None
     return a * b
 
 
